09c063ce2930af47c2108c5a35fd026bc4dc310e
Phil Wornath <philipp@wornath.de>
2022-10-12 01:22:02 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,24 +1,36 @@
 import getNewFeedItems from './feed';
 import {
+  getExistingPages,
   addFeedItemToNotion,
-  deleteOldUnreadFeedItemsFromNotion,
+  deleteOldUnreadItemsFromNotion,
 } from './notion';
 import htmlToNotionBlocks from './parser';
 
 async function index() {
   const feedItems = await getNewFeedItems();
+  const existingPages = await getExistingPages(feedItems);
 
   for (let i = 0; i < feedItems.length; i++) {
     const item = feedItems[i];
+
+    const existingEntries = existingPages.find(
+      (page) => page.properties.Link.url === item.link
+    );
+
+    const isNewEntry = existingEntries === undefined;
+
     const notionItem = {
       title: item.title,
       link: item.link,
       content: htmlToNotionBlocks(item.content),
     };
-    await addFeedItemToNotion(notionItem);
+
+    if (isNewEntry) {
+      await addFeedItemToNotion(notionItem);
+    }
   }
 
-  await deleteOldUnreadFeedItemsFromNotion();
+  await deleteOldUnreadItemsFromNotion();
 }
 
 index();
